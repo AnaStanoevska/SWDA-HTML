693f8fe25427577d456c7f04146a23b897863f24
AnaStanoevska <anastanoevska92@gmail.com>
2019-02-08 01:34:57 +0100

    .

---
+++ js/inputRange.js
@@ -0,0 +1,10 @@
+var A = document.querySelector('.slider');
+var b = document.querySelector('.kocka');
+
+
+calc.addEventListener('change', function(){
+    for ( var A = 10; A <= 100; A + 10) {
+
+    }
+    return b  == A
+})
