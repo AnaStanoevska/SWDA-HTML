f43e0c08cacd95b478d859eb321bd5708a5669d3
AnaStanoevska <anastanoevska92@gmail.com>
2019-02-10 14:33:10 +0100

    nope

---
+++ js/zadaci/BMI.js
@@ -0,0 +1,9 @@
+function calc () {
+var height = document.querySelector('.m');
+var weight = document.querySelector('.kg');
+var submit = weight / (height * height);
+
+
+document.querySelector(".submit");
+console.log(submit);
+}
