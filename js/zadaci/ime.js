a4ccb6482f4d4ad9777a3c8ffb0ced3ff1ff4d3a
AnaStanoevska <anastanoevska92@gmail.com>
2019-02-11 20:58:52 +0100

    nope

--- js/zadaci/ime.js
+++ js/zadaci/ime.js
@@ -1,21 +1,18 @@
 var iminja = ['Ана','Марија','Весна','Aлександра'];
+function iminja() {
+    var najdolgo = iminja [0];
+    var najkratko = iminja [0];
 
-var min = iminja[0];
-var max = iminja[0];
+    for(var i = 0; i < iminja.length; i++) {
+        if(iminja[i].length > najdolgo.length) {
+            najdolgo = iminja[i];
+        }
 
-for (int i = 1; i < iminja.length; i++) 
-    if (iminja[i] > max) 
-    {
-        max = iminja[i];
+        if(iminja[i].length < najkratko.length) {
+            najkratko = iminja[i];
+        }
     }
-    if (iminja[i] < min) 
-    {
-        min = iminja[i];
-    }
-}
 
-System.out.println("Largest Number in a given array is : " + max);
-System.out.println("Smallest Number in a given array is : " + min);
-}
-
+    console.log(najdolgo);
+    console.log(najkratko);
 }
