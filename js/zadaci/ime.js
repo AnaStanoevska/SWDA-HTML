e106e6b341e8677d4d40b25e4384157798b4a9aa
AnaStanoevska <anastanoevska92@gmail.com>
2019-02-11 19:42:58 +0100

    &&
    &&

--- js/zadaci/ime.js
+++ js/zadaci/ime.js
@@ -1,1 +1,21 @@
 var iminja = ['Ана','Марија','Весна','Aлександра'];
+
+var min = iminja[0];
+var max = iminja[0];
+
+for (int i = 1; i < iminja.length; i++) 
+    if (iminja[i] > max) 
+    {
+        max = iminja[i];
+    }
+    if (iminja[i] < min) 
+    {
+        min = iminja[i];
+    }
+}
+
+System.out.println("Largest Number in a given array is : " + max);
+System.out.println("Smallest Number in a given array is : " + min);
+}
+
+}
--- js/zadaci/iminjaLength.js
+++ js/zadaci/iminjaLength.js
@@ -1,9 +1,11 @@
-var ime  = document.querySelector('.ime');
+function baraj() {
+var ime  = document.querySelector('.ime').value;
 
 if(ime.length <= 5){
     console.log('Кратко име');
-} else if(ime.length >=5<=8){
+} else if(ime.length >=5  && ime.length <=8){
     console.log('Име со средна големина');
-} else{
+} else if(ime.length > 8){
     console.log('Долго име');
+}
 }
